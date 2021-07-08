802c363515c066100ef0a51454391a53e45788ec
soheb alam <sohebalam@hotmail.com>
2021-07-08 19:05:38 +0100

    rearrangelesson

--- client/pages/instructor/course/edit/[slug].js
+++ client/pages/instructor/course/edit/[slug].js
@@ -5,7 +5,8 @@ import CourseCreateForm from "../../../../components/forms/CourseCreateForm"
 import Resizer from "react-image-file-resizer"
 import { toast } from "react-toastify"
 import { useRouter } from "next/router"
-
+import Item from "antd/lib/list/Item"
+import { Avatar, List } from "antd"
 const CourseEdit = () => {
   const router = useRouter()
   // state
@@ -17,6 +18,7 @@ const CourseEdit = () => {
     paid: true,
     category: "",
     loading: false,
+    lessons: [],
   })
   const [image, setImage] = useState({})
   const [preview, setPreview] = useState("")
@@ -88,8 +90,33 @@ const CourseEdit = () => {
 
   const loadCourse = async () => {
     const { data } = await axios.get(`/api/course/${slug}`)
-    setValues(data)
+    if (data) {
+      setValues(data)
+    }
+
     if (data && data.image) setImage(data.image)
+  }
+
+  const handelDrag = (e, index) => {
+    // console.log("ON DRAG", index)
+    e.dataTransfer.setData("itemIndex", index)
+  }
+  const handelDrop = async (e, index) => {
+    // console.log("ON DROP", index)
+    const movingItemIndex = e.dataTransfer.getData("itemIndex")
+    const targetItemIndex = index
+    let allLessons = values.lessons
+    let movingItem = allLessons[movingItemIndex]
+    allLessons.splice(movingItemIndex, 1)
+    allLessons.splice(targetItemIndex, 0, movingItem)
+
+    setValues({ ...values, lessons: [...allLessons] })
+    const { data } = await axios.put(`/api/course/${slug}`, {
+      ...values,
+      image,
+    })
+    console.log("Lessons arrange", data)
+    toast("Lessons rearranged")
   }
 
   return (
@@ -111,6 +138,28 @@ const CourseEdit = () => {
       {/* <pre>{JSON.stringify(values, null, 4)}</pre>
       <hr />
       <pre>{JSON.stringify(image, null, 4)}</pre> */}
+      <div className="row pb-5">
+        <div className="col lesson-list">
+          <h4>{values && values.lessons && values.lessons.length} Lessons</h4>
+          <List
+            onDragOver={(e) => e.preventDefault()}
+            itemLayout="horizontal"
+            dataSource={values && values.lessons}
+            renderItem={(item, index) => (
+              <Item
+                draggable
+                onDragStart={(e) => handelDrag(e, index)}
+                onDrop={(e) => handelDrop(e, index)}
+              >
+                <Item.Meta
+                  avatar={<Avatar>{index + 1}</Avatar>}
+                  title={item.title}
+                ></Item.Meta>
+              </Item>
+            )}
+          ></List>
+        </div>
+      </div>
     </InstructorRoute>
   )
 }
